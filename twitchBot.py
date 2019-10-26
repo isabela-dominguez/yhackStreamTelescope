794812b4015b08cd3db898aeaeabe970898cfbe4
Eric Leask <16efl@queensu.ca>
2019-10-26 14:38:40 -0800

    Cleaning up repo

--- twitchBot.py
+++
@@ -1,6 +0,0 @@
-import twitchstream
-username="Mr_Krobe"
-oauth="oauth:mx2edonm7rifr3alyc1rachs4w5lph"
-from twitchstream.chat import TwitchChatStream
-res=TwitchChatStream(username, oauth, False)
-stream.connect()
