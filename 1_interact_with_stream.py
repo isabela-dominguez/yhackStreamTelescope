4e4e81f4a147287bfea5e0d4761c778d08deab5d
isabela-dominguez <isabela.dohu@yahoo.com.mx>
2019-10-26 13:48:06 -0400

    message parsed

--- 1_interact_with_stream.py
+++ 1_interact_with_stream.py
@@ -2,8 +2,10 @@ import twitch
 import socket 
 import logging
 from emoji import demojize
+import re
 
 #--------------- STREAM twitch using SOCKETS
+# source: https://www.learndatasci.com/tutorials/how-stream-text-data-twitch-sockets-python/
 
 # parameters 
 server = 'irc.chat.twitch.tv'
@@ -11,13 +13,6 @@ port = 6667
 nickname = 'is1bela'
 token = 'oauth:yb5y1pc3eeuncy1y5hjms04w7irew7'
 channel = '#mr_krobe'
-
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s — %(message)s',
-                    datefmt='%Y-%m-%d_%H:%M:%S',
-                    handlers=[logging.FileHandler('chat.log', encoding='utf-8')])
-
 
 def main():
     # socket initialization 
@@ -35,7 +30,11 @@ def main():
                 # sock.send("PONG :tmi.twitch.tv\n".encode('utf-8'))
                 sock.send("PONG\n".encode('utf-8'))
             elif len(resp) > 0:
-                logging.info(demojize(resp))
+                if "PRIVMSG" in resp: 
+                    username, channel_from, message = re.search(':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', demojize(resp)).groups()
+                    message = message.replace('\r', '')
+                    print(message)
+
 
     except KeyboardInterrupt:
         sock.close()
