bf88b5bcd6fbb33dc82802a0cffcd6bf50d77bbf
Eric Leask <16efl@queensu.ca>
2019-10-26 18:10:56 -0800

    updating servos to read coordinates

--- receiveFromPython/receiveFromPython.ino
+++ receiveFromPython/receiveFromPython.ino
@@ -1,24 +1,55 @@
 #include <Servo.h>
-Servo myservo;  // create servo object to control a servo
-
-void setup() {
+Servo tiltServo;
+Servo panServo; // create servo object to control a servo
+int panServoPin = 10;
+int panDegree = 90;
+int tiltServoPin = 9;
+int tiltDegree = 90;
+void setup()
+{
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  myservo.attach(9);
+  tiltServo.attach(tiltServoPin);
+  panServo.attach(panServoPin);
 }
 // the loop routine runs over and over again forever:
-void loop() {
+void loop()
+{
   //read from serial
-  String command=Serial.readString();
-  myservo.write(175);
-  //if a command is found in the serial buffer 
-  if (command.length()>0){
-    myservo.write(90);
+  //if a command is found in the serial buffer
+  if (Serial.available() > 0)
+  {
+    //read value
+    String command = Serial.readStringUntil(';');
+    tiltServo.write(175);
+    sleep(500)
+    //is it a move command?
+  if (command.compare("move"){
+      //get coordinates
+      tiltServo.write(135);
+      sleep(500)
+      x = int(Serial.readStringUntil(';'));
+      y = int(Serial.readStringUntil(';'));
   }
-  //output to serial monitor 
-  
-  delay(500);        // delay in between reads for stability
+  //output to servo motors
+  moveServo(x,y);
+  }
+  delay(100); // delay in between reads for stability
 }
 
+void moveServoRelative(int x, int y)
+{
+  panDegree += x;
+  tiltDegree += y;
+  panServo(panDegree);
+  tiltServo(tiltDegree);
+}
+void moveServo(int x, int y)
+{
+  panDegree = x;
+  tiltDegree = y;
+  panServo(panDegree);
+  tiltServo(tiltDegree);
+}
 //void serialEvent() {
 //}
